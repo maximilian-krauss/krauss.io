aff0aa0e8c633f2bf95903a48b19b79704f77760
Maximilian Krauss <max@krauss.io>
2015-07-22 23:25:41 +0200

    Added caching of blog posts

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,19 @@
+var cache = require('memory-cache'),
+    thresholdInMS = 10 * 60 * 1000; //5 Minutes
+
 module.exports = function() {
   var app = this.app,
       core = this.core;
 
   app.get('/', function(req, res) {
 
+    var cachedItems = cache.get('postItems');
+    if(cachedItems) {
+      return res.render('home', { posts: cachedItems })
+    }
+
     core.feedFetcher(function(err, postItems) {
+      cache.put('postItems', postItems, 5000);
       res.render('home', {
         posts: postItems
       });
