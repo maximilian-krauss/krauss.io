a7a50711d5c014d73626543b9b7ab1acf632c645
Maximilian Krauss <max@krauss.io>
2015-09-05 16:24:43 +0200

    Added project structure

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -1,10 +1,13 @@
+var projectStack = require('../../content/projects.json');
+
 module.exports = function() {
   var app = this.app,
       core = this.core;
 
   app.get('/projects', function(req, res) {
     res.render('projects', {
-      subTitle: 'Projects'
+      subTitle: 'Projects',
+      projects: projectStack
     })
   });
 };
