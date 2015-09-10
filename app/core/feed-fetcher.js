1980623d9a1413f4615ab5fe32764dec980ea348
Maximilian Krauss <max@krauss.io>
2015-09-10 22:26:15 +0200

    New feed parser, node 4.0 is required

--- app/core/feed-fetcher.js
+++ app/core/feed-fetcher.js
@@ -1,26 +1,26 @@
 var _ = require('lodash'),
-    parser = require('rssparser'),
-    str = require('string');
+    parser = require('feed-read'),
+    moment = require('moment');
 
 var feedUrl = 'http://blog.krauss.io/rss/';
 
 module.exports = function(cb) {
 
-  var options = {};
-  parser.parseURL(feedUrl, options, function(err, out) {
+  parser(feedUrl, (err, articles) => {
     if(err) {
       return cb(err);
     }
 
-    var posts = [];
-
-    _.forEach(out.items, function(item) {
-      posts.push({
-        postTitle: str(item.title).unescapeHTML().s,
-        timeAgo: item.time_ago,
-        url: item.url
-      });
-    });
+    var posts = _(articles)
+      .chain()
+      .map((a) => {
+        return {
+          postTitle: a.title,
+          timeAgo: moment(a.published).fromNow(),
+          url: a.link
+        };
+      })
+      .value();
 
     cb(null, posts);
   });
